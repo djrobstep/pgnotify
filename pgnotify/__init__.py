72a8560920216fb810e2298b2316f4044fad2316
Robert Lechte <robertlechte@gmail.com>
2018-11-02 22:57:43 +1100

    add null logging handler

--- pgnotify/__init__.py
+++ pgnotify/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-
+from logx import log
+log.set_null_handler()
 from .notify import await_pg_notifications, get_dbapi_connection  # noqa
-
-__all__ = ["await_pg_notifications"]
